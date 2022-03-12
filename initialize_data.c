6d88ec926c9b933601aedb830097939ee1c68175
JosephMamani <jmmollenedo@gmail.com>
2022-03-12 01:05:55 +0000

    fixed minor bug in swap function

---
+++ initialize_data.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * inicialize_data - inicialize the struct with the info of the program
+ * @data: pointer to the structure of data
+ * @UNUSED: array of arguments pased to the program execution
+ * @argv: number of values received from the command line
+ */
+void inicialize_data(data_of_program *data, int argc UNUSED, char *argv[])
+{
+	 stack_p **stack = NULL;
+	 stack_p *head = NULL;
+  /* just 'monty' on the command line or more than two arguments */
+	data->file_path = argv[1];
+	data->line_number = 0;
+	data->lines = NULL;
+	data->words = NULL;
+	data->stack = stack;
+	data->head = head;
+}
